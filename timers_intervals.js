e7ce4774f15e734593900af46188800a3ba68704
zivattias <ziv.attias7@gmail.com>
2023-02-12 10:17:32 +0200

    functional programming w/ setTimeout & clearTimeout

---
+++ timers_intervals.js
@@ -0,0 +1,11 @@
+// Functional programming a clearTimeout after a setTimeout has been called:x
+function start(event) {
+    document.getElementById("finished").innerText = 'timer started'
+    seconds = document.getElementById("inputSec").value
+    console.log("setting timer for", seconds)
+    timerID = setTimeout(
+        () => { document.getElementById("finished").innerText = 'done' },
+        seconds * 1000
+    )
+    document.getElementById("cancelButton").onclick = () => clearTimeout(timerID)
+}
