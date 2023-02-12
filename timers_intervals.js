c441e7692b628ab2541e370463cf237e7a0667eb
zivattias <ziv.attias7@gmail.com>
2023-02-12 13:25:56 +0200

    camelCase fix

--- timers_intervals.js
+++ timers_intervals.js
@@ -1,11 +1,11 @@
-// Functional programming a clearTimeout after a setTimeout has been called:
+// Functionally program a clearTimeout after a setTimeout has been called:
 function start(event) {
     document.getElementById("finished").innerText = 'timer started'
     seconds = document.getElementById("inputSec").value
     console.log("setting timer for", seconds)
-    timerID = setTimeout(
+    timerId = setTimeout(
         () => { document.getElementById("finished").innerText = 'done' },
         seconds * 1000
     )
-    document.getElementById("cancelButton").onclick = () => clearTimeout(timerID)
+    document.getElementById("cancelButton").onclick = () => clearTimeout(timerId)
 }
